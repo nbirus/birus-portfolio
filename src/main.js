3ae17e806dcdba1df5add3b22ca3bae7cbb746d1
Nathan Birus <nathan.birus@nih.gov>
2019-12-28 01:37:14 -0600

    remove

--- src/main.js
+++ src/main.js
@@ -5,22 +5,6 @@ import router from './router'
 // styles
 import './styles/main.scss'
 
-// import the plugin
-// import VueGAPI from 'vue-gapi'
-
-// create the 'options' object
-// const apiConfig = {
-// apiKey: '<YOUR_API_KEY>',
-// clientId: '1012967258727-ojoeajtd00pchsmuaf05aqk4oh3f4o08.apps.googleusercontent.com',
-// scope: 'email profile openid',
-// scope: 'https://www.googleapis.com/auth/photoslibrary.readonly',
-// scope: 'https://www.googleapis.com/auth/spreadsheets',
-// see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
-// }
-
-// Use the plugin and pass along the configuration
-// Vue.use(VueGAPI, apiConfig)
-
 Vue.config.productionTip = false
 
 new Vue({
