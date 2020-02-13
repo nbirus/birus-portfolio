d48cedc09e6553850db42520a32789b8b653e433
Nathan Birus <nbirus@harding.edu>
2020-02-13 00:11:56 -0600

    m

--- src/components/photography/PhotographyTagSlider.vue
+++ src/components/photography/PhotographyTagSlider.vue
@@ -135,6 +135,7 @@ function clone(o) {
 		border: none;
 		transition: box-shadow 0.1s ease;
 		cursor: pointer;
+		background-color: #fff;
 
 		&:hover {
 			box-shadow: 0px 3px 12px rgba(0, 0, 0, 0.25);
