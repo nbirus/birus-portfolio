673df69da9a15c083ef601f09b72b8a506ae548a
Nathan Birus <nathan.birus@nih.gov>
2020-02-16 13:17:47 -0600

    small improvments

--- src/views/pages/photography/PhotoPage.vue
+++ src/views/pages/photography/PhotoPage.vue
@@ -338,7 +338,7 @@ function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
 	&__img {
 		max-height: 100%;
 		max-width: 100%;
-		animation: pop-in 0.3s ease;
+		animation: pop-in 0.75s ease;
 	}
 
 	// controls
@@ -435,9 +435,15 @@ function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
 }
 @keyframes pop-in {
 	0% {
+		opacity: 0;
+		transform: scale(0.75);
+	}
+	50% {
+		opacity: 0;
 		transform: scale(0.75);
 	}
 	100% {
+		opacity: 1;
 		transform: scale(1);
 	}
 }
